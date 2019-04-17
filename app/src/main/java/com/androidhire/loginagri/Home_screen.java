6e8b4c0ee3ed46073d8c11831044f8e96eb5c6d0
Patrick <natharc.nt@gmail.com>
2019-04-17 17:05:47 -0500

    Se agregó 3 strings a strings.xml Creación 3 fragments (Home, Dashboard y Notifications) Se comentó todas las lineas que hacen referencia a LOGOUT Se cortó las lineas de home_screen_activity hacia Home_screen para que no interfiera la función onStart(), por que si no, no funciona la app

--- app/src/main/java/com/androidhire/loginagri/Home_screen.java
+++ app/src/main/java/com/androidhire/loginagri/Home_screen.java
@@ -3,6 +3,7 @@ package com.androidhire.loginagri;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,52 +13,60 @@ import android.support.design.widget.BottomNavigationView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Home_screen extends AppCompatActivity {
+public class Home_screen extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    FirebaseAuth mAuth;
+    /*FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListner;
 
     @Override
     protected void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListner);
-    }
+    }*/
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen_activity);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
 
-        Button button = (Button) findViewById(R.id.signout);
-        mAuth = FirebaseAuth.getInstance();
+        loadFragment(new HomeFragment());
 
-        mAuthListner = new FirebaseAuth.AuthStateListener() {
+        //Button button = (Button) findViewById(R.id.signout);
+        // Copiar en el archivo home_screen_activity
+        /*<TextView
+        android:id="@+id/textView"
+        android:layout_width="0dp"
+        android:layout_height="wrap_content"
+        android:layout_marginStart="24dp"
+        android:layout_marginEnd="24dp"
+        android:lineSpacingExtra="8sp"
+        android:text="@string/login_exito"
+        android:textSize="36sp"
+        android:textStyle="bold"
+        app:layout_constraintBottom_toTopOf="@+id/signout"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent" />
+
+    <Button
+        android:id="@+id/signout"
+        android:layout_width="0dp"
+        android:layout_height="wrap_content"
+        android:layout_marginStart="24dp"
+        android:layout_marginEnd="24dp"
+        android:text="@string/LogOut"
+        app:layout_constraintBottom_toTopOf="@+id/navigation"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent" />*/
+        //mAuth = FirebaseAuth.getInstance();
+
+        /*mAuthListner = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if (firebaseAuth.getCurrentUser()==null)
@@ -72,8 +81,42 @@ public class Home_screen extends AppCompatActivity {
             public void onClick(View v) {
                 mAuth.signOut();
             }
-        });
+        });*/
 
 
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if( fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch (item.getItemId()){
+
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_dashboard:
+                fragment = new DashboardFragment();
+                break;
+
+            case R.id.navigation_notifications:
+                fragment = new NotificationFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
     }
 }
